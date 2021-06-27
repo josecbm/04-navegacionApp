1ebe501c22b6c4db57df8ebcd39c9aa37be04127
josecbm <jcbautista95@gmail.com>
2021-06-27 10:46:06 -0600

    asignarle el tipo rapido FORMA 1

--- src/screens/PersonaScreen.tsx
+++ src/screens/PersonaScreen.tsx
@@ -3,13 +3,19 @@ import { Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 import { StackScreenProps } from '@react-navigation/stack';
 
+interface RouterParams {
+    id: number,
+    nombre: string
+}
 
 interface Props extends StackScreenProps<any,any>{};
+
+
 export const PersonaScreen = ({navigation,route}: Props) => {
-    const params = route.params;
+    const params = route.params as RouterParams;
     useEffect(() => {
         navigation.setOptions({
-            title: params!.nombre as any
+            title: params.nombre 
         });
     }, [])
     return (
